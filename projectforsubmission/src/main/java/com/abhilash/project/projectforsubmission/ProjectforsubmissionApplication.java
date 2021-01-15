593192d61c6e7cc1c8f8323659b15cd7efefce2a
abhilash0704 <77488385+abhilash0704@users.noreply.github.com>
2021-01-15 19:26:00 +0530

    Add files via upload

---
+++ projectforsubmission/src/main/java/com/abhilash/project/projectforsubmission/ProjectforsubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.abhilash.project.projectforsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectforsubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectforsubmissionApplication.class, args);
+	}
+
+}
